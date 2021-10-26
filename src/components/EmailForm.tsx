2407dc7d4ced199c7435d7686fe7e3485f8bf954
Cobular <22972550+JakeCover@users.noreply.github.com>
2021-10-25 20:14:21 -0700

    got the basics going

---
+++ src/components/EmailForm.tsx
@@ -0,0 +1,80 @@
+import React, {useState} from "react";
+import email_white_36dp from "../static/landing/email-white-36dp.svg"
+import check from "../static/landing/check.svg"
+import cross from "../static/landing/cross.svg"
+
+export const EmailForm = () => {
+  const [mailChimpResponse, setMailChimpResponse] = useState("");
+
+  const addEmail = (event: any) => {
+    if(event.key !== 'Enter') {
+      return;
+    }
+
+    // @ts-ignore
+    const emailInput = document.getElementById("email-input").value;
+
+    fetch('/v1/email', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: emailInput,
+      }),
+    }).then((res) => {
+      return res.json();
+    })
+        .then((json) => {
+          let response = "";
+          response = json.message;
+          if (!json.success) {
+            if (json.reason) {
+              response += ': ' + json.reason;
+            }
+          }
+          setMailChimpResponse(response);
+        });
+  }
+
+  return (
+      <div className="email-form">
+        <p id="join-line">Join 1000+ makers, innovators, and dreamers.</p>
+        <p id="sub-join-line">Be the first to know when applications are live!</p>
+
+        {mailChimpResponse !== "Confirmed" && (
+            <div className="form-container">
+              <div className="form">
+                <div className="form-email">
+                  <img src={email_white_36dp} />
+                  <input id="email-input" placeholder="Enter your email" onKeyDown={addEmail}/>
+                </div>
+                {mailChimpResponse === "" && (
+                    <div className="chip">
+                      <span>Press return</span>
+                      <img className="form-icon" src={check}/>
+                    </div>
+                )}
+                {mailChimpResponse === "Invalid email" && (
+                    <div className="chip">
+                      <span>{mailChimpResponse}</span>
+                      <img className="form-icon" src={cross}/>
+                    </div>
+                )}
+              </div>
+              {mailChimpResponse !== "Invalid email" && mailChimpResponse !== "" && (
+                  <div className="chip">
+                    <span>{mailChimpResponse}</span>
+                  </div>
+              )}
+            </div>
+        )}
+
+        {mailChimpResponse === "Confirmed" && (
+            <div id="confirmed-box">
+              <span id="confirmation-text">Thanks, see you soon!</span>
+            </div>
+        )}
+      </div>
+  );
+};
---
+++ src/components/Title.tsx
@@ -0,0 +1,18 @@
+import React  from 'react';
+import title300 from "../static/landing/title-300w.svg"
+import title700 from "../static/landing/title-700w.svg"
+
+export const Title = () => {
+  return (
+      <div className="title-container">
+        <img
+            id="title"
+            srcSet={`${title300} 300w,
+                    ${title700} 800w`}
+            sizes="(max-width: 900px) 300px, 800px"
+            src={title700}
+            alt="LA Hacks 2021, March 21-28 2021"
+        />
+      </div>
+  );
+};
