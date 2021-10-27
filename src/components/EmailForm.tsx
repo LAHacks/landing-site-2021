406cc7b26955485bb9954c78910795731fb9fac3
Sophia Adrangi <sophiaadrangi@gmail.com>
2021-10-26 23:35:18 -0700

    mailChimpResponse differences in display cleaned up in code logic

--- src/components/EmailForm.tsx
+++ src/components/EmailForm.tsx
@@ -1,7 +1,7 @@
 import React, { MutableRefObject, useRef, useState} from "react";
 import email_white_36dp from "../static/landing/email-white-36dp.svg"
-//import check from "../static/landing/check.svg"
-//import cross from "../static/landing/cross.svg"
+import check from "../static/landing/check.svg"
+import cross from "../static/landing/cross.svg"
 
 export const EmailForm = () => {
   const [mailChimpResponse, setMailChimpResponse] = useState("");
@@ -9,9 +9,7 @@ export const EmailForm = () => {
   const emailInput = useRef() as MutableRefObject<HTMLInputElement>;
   
   const addEmail = (event: any) => {
-    /*if(event.key !== 'Enter') {
-      return;
-    }*/
+   
     event.preventDefault()
     
     
@@ -43,17 +41,36 @@ export const EmailForm = () => {
           setMailChimpResponse(response);
         });
   }
-
-  
-  return (
-      <div className="email-form">
-        <p id="join-line">Join 1000+ makers, innovators, and dreamers.</p>
-        <p id="sub-join-line">Be the first to know when applications are live!</p>
-
-        <div className="form-container">
-          <div className="form">
-            <form onSubmit={addEmail}>
-              <fieldset>
+  const userErrorFiller = () => {
+    if (mailChimpResponse === "Invalid email") {
+      return (
+        <div className="chip">
+          <span> {mailChimpResponse}</span>
+          <img className="form-icon" src={cross} />
+        </div>
+      );
+    }
+    else if (mailChimpResponse !== "Invalid email" && mailChimpResponse !== "") {
+      return (
+      <div className="chip">
+        <span>{mailChimpResponse}</span>
+        </div>
+      ); 
+    }
+  }
+  const determineFiller = () => {
+    if (mailChimpResponse === "Confirmed"){
+      return <div id="confirmed-box">
+      <span id="confirmation-text">Thanks, see you soon!</span>
+      </div>
+    }
+    else {
+      return (
+      <div className="form-container">
+        <div className="form">
+          <div className="form-email">
+          <form onSubmit={addEmail}>
+              
                 <label className="form-email">
                   
                   <img src={email_white_36dp} alt="envelope"/>
@@ -63,50 +80,24 @@ export const EmailForm = () => {
                     ref={emailInput}
                     placeholder="Enter your email"
                   />
-
+                  {userErrorFiller()}
                 </label>
-            </fieldset>
+            
+                
             <button type="submit"> Submit :-) </button>
             </form>
           </div>
 
         </div>
-        
-        {/*
-        {mailChimpResponse !== "Confirmed" && (
-            <div className="form-container">
-              <div className="form">
-                <div className="form-email">
-                  <img src={email_white_36dp} />
-                  <input id="email-input" placeholder="Enter your email" onKeyDown={addEmail}/>
-                </div>
-                {mailChimpResponse === "" && (
-                    <div className="chip">
-                      <span>Press return</span>
-                      <img className="form-icon" src={check}/>
-                    </div>
-                )}
-                {mailChimpResponse === "Invalid email" && (
-                    <div className="chip">
-                      <span>{mailChimpResponse}</span>
-                      <img className="form-icon" src={cross}/>
-                    </div>
-                )}
-              </div>
-              {mailChimpResponse !== "Invalid email" && mailChimpResponse !== "" && (
-                  <div className="chip">
-                    <span>{mailChimpResponse}</span>
-                  </div>
-              )}
-            </div>
-        )}
-
-        {mailChimpResponse === "Confirmed" && (
-            <div id="confirmed-box">
-              <span id="confirmation-text">Thanks, see you soon!</span>
-            </div>
-        )}
-        */}
-    </div>
+      </div>); 
+    }
+  }
+  return (
+      <div className="email-form">
+        <p id="join-line">Join 1000+ makers, innovators, and dreamers.</p>
+        <p id="sub-join-line">Be the first to know when applications are live!</p>
+        {determineFiller()}
+      </div>
+     
   );
 };
