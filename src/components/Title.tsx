fc0eb7f22c1e4352bae2e86512baa54e72026fd4
Cobular <22972550+JakeCover@users.noreply.github.com>
2021-11-01 22:42:10 -0700

    Updated date

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import title300 from "../static/landing/title-300w.svg"
 import title700 from "../static/landing/title-700w.svg"
 import moon from "../static/landing/moon.svg"
 import leftStars from "../static/landing/LeftStars.svg"
@@ -11,8 +10,6 @@ export const Title = () => {
       <div className="title-container">
         <img
             id="title"
-            srcSet={`${title300} 300w,
-                    ${title700} 800w`}
             sizes="(max-width: 900px) 300px, 800px"
             src={title700}
             alt="LA Hacks 2021, March 21-28 2021"
